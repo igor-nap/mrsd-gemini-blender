01b5fb0a5dd2cf25e8ea000c3b4cc37c684f6b8b
mbhala <bhalakumaran.m@gmail.com>
2013-11-24 01:38:37 -0500

    Python Script for Pointers

--- BlenderPointers/fallDemo.py
+++ BlenderPointers/fallDemo.py
@@ -1,17 +1,17 @@
 import jpc
 import time
 from bge import logic
-from bge import events
 import mathutils
 
-stepFile = open('/home/bhala/DevTools/mrsd-gemini/BlenderPointers/gemini_steps1.txt')
-logic.LibLoad("/home/bhala/DevTools/mrsd-gemini/BlenderPointers/animateObjects.blend","Mesh")
+class Pointers:
+	arrow = 'arrowFlat'
+	circle = 'circleArrow'
+	cross = 'crossHair'
 
-meshCount = 0
 step_num = -1
 
-#List of STL Files stored according to the step IDs"
-meshList = ['arrowFlat','circleArrow','crossHair']
+#Parse the pre-compiled text file to receive data
+stepFile = open('/home/bhala/DevTools/mrsd-gemini-blender/BlenderPointers/gemini_steps1.txt')
 stepFileLine = list(stepFile)
 
 stepNo =[0 for x in range(len(stepFileLine))]
@@ -34,25 +34,34 @@ for lineNo,line in enumerate(stepFileLine):
 	camPos[lineNo] = [float(thisLine[9]),float(thisLine[10]),float(thisLine[11])]
 	camAngles[lineNo] = [float(thisLine[12]),float(thisLine[13]),float(thisLine[14])]
 
+#Get all the pointers - Load it from the blender File
+logic.LibLoad("/home/bhala/DevTools/mrsd-gemini-blender/BlenderPointers/animateObjects.blend","Scene")
+
+#Get the initial Scene
+scene = logic.getCurrentScene()	
+
+objList = scene.objects
+objList[0].visible = False
+objList[1].visible = True
+objList[2].visible = True
+			
+objListStr = [str(x) for x in objList]
 
 # Set Initial Camera View to Top View
-scene = logic.getCurrentScene() 
 cam = scene.active_camera 
-
 camPose = mathutils.Euler((0,0,0),'XYZ') 
 cam.worldPosition = [0, 0, 20]
 cam.worldOrientation = camPose
 
-print ('Connecting to Server..')
+#Connect to JSON Server
+# print ('Connecting to Server..')
 c = jpc.connect(host = 'localhost',port=50000)
 s = jpc.Proxy(c)
-print ('Connected')
-print(animateIndex)
+# print ('Connected')
 
 def renderStep():
 	cubeController = logic.getCurrentController()
 	cubeOwn = cubeController.owner
-
 
 	def Init():
 		if not 'init' in cubeOwn:
@@ -60,42 +69,72 @@ def renderStep():
 
 	#Load the meshes corresponding to the step_num		
 	def renderObject(step_num):
+		global objList
 		scene = logic.getCurrentScene()
-		cube = scene.objects['Cube']
+		cube = scene.objects[0]
 
-		pointInd = animateIndex[step_num[0]]
-		print(pointInd)
+		pointerIdx = animateIndex[step_num[0]]
 
-		mesh = meshList[animateIndex[step_num[0]]]
-		cube.replaceMesh(mesh)
-		cube.localScale.x = 0.03
-		cube.localScale.z = 0.03
-		cube.localScale.y = 0.03
-		# cube.localPosition.y  = -2
-		# cube.localPosition.x  = -2
+		if pointerIdx == 0:
+			pointer = Pointers.arrow
+		elif pointerIdx == 1:
+			pointer = Pointers.circle
+		else:
+			pointer = Pointers.cross
 
+		print ("Pointer is " + pointer)
 
+		if pointer in objList:
+			objIdx = objListStr.index(pointer)
+			print ("Object Index " + str(objIdx))
+		else:
+			print("Pointer not Found")
+		
+
+		if objIdx == 3:
+		 	objList[3].visible = True
+		 	objList[4].visible = False
+		 	objList[5].visible = False
+		elif objIdx == 4:
+			objList[3].visible = False
+			objList[4].visible = True
+			objList[5].visible = False
+		else:
+			objList[3].visible = False
+			objList[4].visible = False
+			objList[5].visible = True
+
+		# for i in range(0,len(objList)):
+		# 	if i == objIdx:
+		# 		objList[objIdx].visible = True
+		# 		print("In Initial")
+		# 	else:
+		# 		objList[objIdx].visible = False
+		# 		print("offscreen")
+		
 	#Get the state from JSON Server
 	def getState():
 
-		global meshList,meshCount
 		global step_num, s
 
 		curr_state_msg = s.req_state()
-		# curr_state = curr_state_msg[0]
-		curr_state = 1
-		# print (curr_state)
+		curr_state = curr_state_msg[0]
+		# curr_state = 1
+		print (curr_state)
 
-		print ('State variable got :' + str(curr_state))
+		print ('Got State variable :' + str(curr_state))
 
 		#Act based on State
 		if curr_state == 0:					# Server initialized
 			print ("State is initialized. Standing by...")
+			objList[3].visible = False
+			objList[4].visible = False
+			objList[5].visible = False
+
 		elif curr_state == 1: 				# Step requested
 		
 			step_num = s.req_step_n()
-			step_num[0] = 2
-			 #Delete this[]
+			step_num[0] = 3 #Delete this[]
 			s.update_state(2)
 		
 			print ("Rendering...")
@@ -106,8 +145,15 @@ def renderStep():
 			s.update_state(3)
 		
 		elif curr_state == 2:				# Rendering...
+			objList[3].visible = False
+			objList[4].visible = False
+			objList[5].visible = False
 			print ("[Error] gem_blender_json: Why am I here?")
+		
 		elif curr_state == 3:				# Render complete, stand by
+			objList[3].visible = False
+			objList[4].visible = False
+			objList[5].visible = False
 			print ("Standing by...")
 	
 
